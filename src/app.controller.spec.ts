7e80c4d5e4838f3badec8d8f78656b41b3068210
maikcodes <willymromero@gmail.com>
2025-11-18 20:32:24 -0500

    first commit

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,38 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('ping', () => {
+    it('should return "pong" when ping is called', () => {
+      expect(appController.ping()).toBe('pong');
+    });
+  });
+
+  describe('pong', () => {
+    it('should return "ping" when pong is called', () => {
+      expect(appController.pong()).toBe('ping');
+    });
+  });
+
+  describe('game session', () => {
+    it('should return "pong" when ping is called', () => {
+      expect(appController.ping()).toBe('pong');
+    });
+
+    it('should return "ping" when pong is called', () => {
+      expect(appController.pong()).toBe('ping');
+    });
+  });
+});
---
+++ src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  ping(): string {
+    return 'pong';
+  }
+
+  pong(): string {
+    return 'ping';
+  }
+}
